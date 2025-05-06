c82054401a037e74b9d28484d7379f6b1d0e00e0
pavithra2441543 <pavithra.b@bcah.christuniversity.in>
2025-05-06 14:54:36 +0530

    Add files via upload

---
+++ task1/script.js
@@ -0,0 +1,9 @@
+// Scroll effect for navbar
+const navbar = document.getElementById('navbar');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
